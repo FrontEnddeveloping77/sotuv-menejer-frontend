7bf7c3b483af1f200b76faaeee1282bf4d6ac820
abdulazizavazov2508@gmail.com <abdulazizavazov2508@gmail.com>
2026-08-13 11:37:18 +0500

    sayt yangilandi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Base yo'li to'g'ri ko'rsatilgan bo'lishi shart
 })
